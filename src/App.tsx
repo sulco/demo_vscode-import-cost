c2e59788ad0d688d49fe8cec4bde4ec19f2115cb
sulco <tomek.sulkowski@gmail.com>
2024-01-04 14:13:03 +0100

    set up codebase for demoing the extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// turn on `vscode-import-cost` and compare this:
+import { kebabCase } from "lodash"
+// to this:
+import kebabCase_ from "lodash/kebabCase"
+
+// You can confirm these numbers by commenting out 
+// each of the <p>...</p> below and then running `npm run build`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{kebabCase('#1: Vite + React')}</p>
+      <p>{kebabCase_('#2: Vite + React')}</p>
     </>
   )
 }
