28d165f9e84ecb043f3b2057ead9d7282ee2cbb5
sulco <tomek.sulkowski@gmail.com>
2024-01-04 15:23:10 +0100

    rephrase comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-// turn on `vscode-import-cost` 
+// turn on `Import Cost` extension
 // and compare this:
-import { kebabCase } from "lodash"
+import { kebabCase } from "lodash";
 // to this:
-import kebabCase_ from "lodash/kebabCase"
+import kebabCase_ from "lodash/kebabCase";
 
-// You can confirm these numbers by commenting out 
+// You can confirm these numbers by commenting out
 // each of the <p>...</p> below and then running `npm run build`
 
 function App() {
   return (
     <>
-      <p>{kebabCase('#1: Vite + React')}</p>
-      <p>{kebabCase_('#2: Vite + React')}</p>
+      <p>{kebabCase("#1: Vite + React")}</p>
+      <p>{kebabCase_("#2: Vite + React")}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
